72840c8e638e9670f8f603c604d695c98055123c
alihardan <alihardanc@gmail.com>
2025-01-02 21:54:37 +0330

    fix: replace Storage methods with native PHP functions (to get rid of https://github.com/thephpleague/flysystem/issues/1336)

--- app/Http/Controllers/LostGameController.php
+++ app/Http/Controllers/LostGameController.php
@@ -23,11 +23,13 @@ class LostGameController extends Controller
 
     public function rename(Request $request)
     {
-        return Storage::disk('public')->move("lost-games/$request->old_filename", "lost-games/$request->new_filename");
+        return rename(config('filesystems.disks.public.root') . '/lost-games/' . $request->old_filename, config('filesystems.disks.public.root') . '/lost-games/' . $request->new_filename);
+        // return Storage::disk('public')->move("lost-games/$request->old_filename", "lost-games/$request->new_filename");
     }
 
     public function destroy($filename)
     {
-        return Storage::disk('public')->delete("lost-games/$filename");
+        return unlink(config('filesystems.disks.public.root') . '/lost-games/' . $filename);
+        // return Storage::disk('public')->delete("lost-games/$filename");
     }
 }
