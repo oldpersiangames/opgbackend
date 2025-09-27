06ac5ad4dae4f28923ea7c38402f4672fa520728
alihardan <alihardanc@gmail.com>
2025-09-27 03:43:34 +0330

    wip

--- database/migrations/2023_06_21_172643_create_games_table.php
+++ database/migrations/2023_06_21_172643_create_games_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('games', function (Blueprint $table) {
             $table->id();
+
+            $table->string('status')->default('draft');
+
             $table->string('slug')->unique();
             $table->string('ia_id')->nullable();
 
